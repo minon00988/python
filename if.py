6751e254164597f690d92c7ab35074550b736ea3
minon00988 <100515881+minon00988@users.noreply.github.com>
2022-02-27 18:20:53 +0700

    Create if.py

---
+++ if.py
@@ -0,0 +1,21 @@
+name=int(input("пч"))
+if name==0:
+   print("?")
+elif name==1:
+   print("w")
+elif  name==2:
+   print("e")
+elif name==3:
+   print("r")
+elif name==4:
+   print("t")
+elif name==5:
+   print("y")
+elif name==6:
+   print("u")
+elif name==7:
+   print("i")
+elif name==8:
+   print("o")
+elif name==9:
+   print("p")
